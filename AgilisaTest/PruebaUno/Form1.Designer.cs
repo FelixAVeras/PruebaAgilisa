138b78a06f6c03b883d6ca4549945d30208c5d16
Felix Veras <fcarvajal44@gmail.com>
2022-07-12 20:23:30 -0400

    Trabajando en la prueba uno

---
+++ AgilisaTest/PruebaUno/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace PruebaUno
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tablamultiplicarInput = new System.Windows.Forms.NumericUpDown();
+            this.aleatorioInput = new System.Windows.Forms.NumericUpDown();
+            this.secuenciaInput = new System.Windows.Forms.NumericUpDown();
+            this.DesgloseInput = new System.Windows.Forms.NumericUpDown();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnSecuencia = new System.Windows.Forms.Button();
+            this.btnAleatorio = new System.Windows.Forms.Button();
+            this.btnDesglose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tablamultiplicarInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aleatorioInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secuenciaInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DesgloseInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tablamultiplicarInput
+            // 
+            this.tablamultiplicarInput.Location = new System.Drawing.Point(16, 28);
+            this.tablamultiplicarInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tablamultiplicarInput.Name = "tablamultiplicarInput";
+            this.tablamultiplicarInput.Size = new System.Drawing.Size(197, 22);
+            this.tablamultiplicarInput.TabIndex = 1;
+            // 
+            // aleatorioInput
+            // 
+            this.aleatorioInput.Location = new System.Drawing.Point(572, 28);
+            this.aleatorioInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.aleatorioInput.Name = "aleatorioInput";
+            this.aleatorioInput.Size = new System.Drawing.Size(197, 22);
+            this.aleatorioInput.TabIndex = 2;
+            // 
+            // secuenciaInput
+            // 
+            this.secuenciaInput.Location = new System.Drawing.Point(301, 28);
+            this.secuenciaInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.secuenciaInput.Name = "secuenciaInput";
+            this.secuenciaInput.Size = new System.Drawing.Size(197, 22);
+            this.secuenciaInput.TabIndex = 3;
+            // 
+            // DesgloseInput
+            // 
+            this.DesgloseInput.Location = new System.Drawing.Point(853, 28);
+            this.DesgloseInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DesgloseInput.Name = "DesgloseInput";
+            this.DesgloseInput.Size = new System.Drawing.Size(197, 22);
+            this.DesgloseInput.TabIndex = 4;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 122);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(196, 416);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(301, 122);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(196, 416);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(572, 122);
+            this.richTextBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(196, 416);
+            this.richTextBox3.TabIndex = 7;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(853, 122);
+            this.richTextBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(196, 416);
+            this.richTextBox4.TabIndex = 8;
+            this.richTextBox4.Text = "";
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(16, 74);
+            this.btnMultiplicar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(197, 28);
+            this.btnMultiplicar.TabIndex = 9;
+            this.btnMultiplicar.Text = "Tabla Multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnSecuencia
+            // 
+            this.btnSecuencia.Location = new System.Drawing.Point(301, 74);
+            this.btnSecuencia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSecuencia.Name = "btnSecuencia";
+            this.btnSecuencia.Size = new System.Drawing.Size(197, 28);
+            this.btnSecuencia.TabIndex = 10;
+            this.btnSecuencia.Text = "Secuencia 0, 1, 1";
+            this.btnSecuencia.UseVisualStyleBackColor = true;
+            this.btnSecuencia.Click += new System.EventHandler(this.btnSecuencia_Click);
+            // 
+            // btnAleatorio
+            // 
+            this.btnAleatorio.Location = new System.Drawing.Point(572, 74);
+            this.btnAleatorio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAleatorio.Name = "btnAleatorio";
+            this.btnAleatorio.Size = new System.Drawing.Size(197, 28);
+            this.btnAleatorio.TabIndex = 11;
+            this.btnAleatorio.Text = "Ordern Aleatorio";
+            this.btnAleatorio.UseVisualStyleBackColor = true;
+            this.btnAleatorio.Click += new System.EventHandler(this.btnAleatorio_Click);
+            // 
+            // btnDesglose
+            // 
+            this.btnDesglose.Location = new System.Drawing.Point(853, 74);
+            this.btnDesglose.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDesglose.Name = "btnDesglose";
+            this.btnDesglose.Size = new System.Drawing.Size(197, 28);
+            this.btnDesglose.TabIndex = 12;
+            this.btnDesglose.Text = "Desglose Billetes";
+            this.btnDesglose.UseVisualStyleBackColor = true;
+            this.btnDesglose.Click += new System.EventHandler(this.btnDesglose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnDesglose);
+            this.Controls.Add(this.btnAleatorio);
+            this.Controls.Add(this.btnSecuencia);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.DesgloseInput);
+            this.Controls.Add(this.secuenciaInput);
+            this.Controls.Add(this.aleatorioInput);
+            this.Controls.Add(this.tablamultiplicarInput);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tablamultiplicarInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aleatorioInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secuenciaInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DesgloseInput)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown tablamultiplicarInput;
+        private System.Windows.Forms.NumericUpDown aleatorioInput;
+        private System.Windows.Forms.NumericUpDown secuenciaInput;
+        private System.Windows.Forms.NumericUpDown DesgloseInput;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnSecuencia;
+        private System.Windows.Forms.Button btnAleatorio;
+        private System.Windows.Forms.Button btnDesglose;
+    }
+}
+
---
+++ AgilisaTest/PruebaUno/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PruebaUno
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Tabla multiplicar
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            int txtNumero = (int)tablamultiplicarInput.Value;
+
+            for (int i = 1; i < txtNumero + 1; i++)
+            {
+                string resultado = string.Empty;
+
+                for (int j = 1; j < 13; j++)
+                {
+                    resultado += i + " x " + j + " = " + (j * i) + "\n";
+                }
+                richTextBox1.Text = resultado;
+            }
+        }
+
+        //Secuencia 0, 1, 1
+        private void btnSecuencia_Click(object sender, EventArgs e)
+        {
+            int firstNumber = 0;
+            int SecondNumber = 1;
+            int nextNumber; 
+            int numberOfElements = (int)secuenciaInput.Value;
+            
+            richTextBox2.Text = firstNumber + "\n" + SecondNumber + "\n";
+            
+            for (int i = 2; i < numberOfElements; i++)
+            {
+                nextNumber = firstNumber + SecondNumber;
+                richTextBox2.Text += nextNumber + "\n";
+                firstNumber = SecondNumber;
+                SecondNumber = nextNumber;
+            }
+        }
+
+        //Orden Aleatorio
+        private void btnAleatorio_Click(object sender, EventArgs e)
+        {
+            Random aleatorio = new Random();
+
+            int txtAleatorio = (int)aleatorioInput.Value;
+            string resultado = string.Empty;
+
+            for (int x = 0; x < txtAleatorio; x++)
+            {
+                resultado += aleatorio.Next(txtAleatorio) + "\n";
+            }
+
+            richTextBox3.Text = resultado;
+        }
+
+        //Desglose de Billetes
+        private void btnDesglose_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
